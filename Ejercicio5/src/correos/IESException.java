3427d58a8d75df155755520b2a262da72d09635a
Angel Martin Gallardo <a.martin.gallardo@iescristobaldemonroy.es>
2026-04-27 13:47:35 +0200

    Add files via upload

---
+++ Ejercicio5/src/correos/IESException.java
@@ -0,0 +1,14 @@
+package correos;
+
+
+
+
+public class IESException  extends Exception{
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public IESException(String msg){
+		super(msg);
+	}
+}
